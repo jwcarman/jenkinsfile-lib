5ce8a66523542390b158b04d9b96713dbb5e911a
James Carman <james.carman@cengage.com>
2018-02-06 09:11:02 -0500

    Refactoring.

--- vars/doWithMaven.groovy
+++ vars/doWithMaven.groovy
@@ -2,7 +2,7 @@ def call(Map config, Closure body) {
 
     jdkTool = config.jdk ?: "JDK8"
     mavenTool = config.maven ?: "Maven 3.5.x"
-    mavenSettings = config.mavenSettings ?: "doWithMaven-settings"
+    mavenSettings = config.mavenSettings ?: "maven-settings"
 
     withMaven(jdk: jdkTool, maven: mavenTool, mavenSettingsConfig: mavenSettings) {
         body()
