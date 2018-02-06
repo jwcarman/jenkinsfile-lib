8e5e473db18c2c14aa9b79db1f2c250be677a35b
James Carman <james.carman@cengage.com>
2018-02-06 09:07:26 -0500

    Refactoring.

--- vars/doCheckout.groovy
+++ vars/doCheckout.groovy
@@ -1,7 +1,10 @@
 def call(Map config) {
+
+    doCheckout scm
+
     gitUserName = config.gitUserName ?: "Jenkins"
     gitUserEmail = config.gitUserEmail ?: "jenkins@caramnconsulting.com"
     sh "git config --local user.name  ${gitUserName}"
     sh "git config --local user.email ${gitUserEmail}"
-    doCheckout scm
+
 }
