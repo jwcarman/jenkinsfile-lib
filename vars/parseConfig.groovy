d47acf3349b4718ad18136a7eead19bff480e26d
James Carman <james.carman@cengage.com>
2018-02-06 09:12:13 -0500

    Refactoring.

--- vars/parseConfig.groovy
+++ vars/parseConfig.groovy
@@ -1,5 +1,8 @@
 def call(Closure body) {
     def config = [:]
+    if (body == null) {
+        return config
+    }
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
