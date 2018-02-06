ded5184f587c16fcec735104353678af7dbb4773
James Carman <james.carman@cengage.com>
2018-02-06 09:03:55 -0500

    Refactoring.

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -1,8 +1,5 @@
-def call(body) {
-    def config = [:]
-    body.resolveStrategy = Closure.DELEGATE_FIRST
-    body.delegate = config
-    body()
+def call(Closure body) {
+    config = parseConfig(body)
 
     node {
         stage('Checkout') {
